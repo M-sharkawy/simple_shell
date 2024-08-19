12379164ecae008b2ce517c24926e4895e4223b7
Martin George <martingeorge191110@gmail.com>
2024-08-19 17:18:25 +0300

    Edit some files

--- shell.c
+++ shell.c
@@ -12,17 +12,15 @@
 int main(int argc, char *argv[], char **env)
 {
 	char *buffer = NULL, **cmdArr;
-	size_t n = 0;
-	ssize_t bytes;
 
 	(void)argc;
 
 	while (1)
 	{
 		print_prompt();
-		bytes = getline(&buffer, &n, stdin);
-		if (bytes > 1)
-			cmdArr = get_command(buffer);
+		buffer = get_line();
+		cmdArr = get_command(buffer);
+
 		if (!cmdArr)
 			continue;
 

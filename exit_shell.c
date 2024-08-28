35419a2bded507fcea584eaa1ecd58b353443be1
M-sharkawy <mostafaeelsharkawy@gmail.com>
2024-08-28 14:35:46 +0300

    update exit file

--- exit_shell.c
+++ exit_shell.c
@@ -17,15 +17,21 @@ int exit_shell(char **cmd_arry, char **env)
 	{
 		command_free(cmd_arry);
 		exit(0);
-	} else
-	{
-		status = _atoi(cmd_arry[1]);
-		if (status < 0)
-			return (-1);
-
-		command_free(cmd_arry);
-		exit(status);
 	}
+
+	status = _atoi(cmd_arry[1]);
+
+    if (status < 0 || status > 255)
+    {
+        write(STDERR_FILENO, "exit: ", 6);
+        write(STDERR_FILENO, cmd_arry[1], str_len(cmd_arry[1]));
+        write(STDERR_FILENO, ": numeric argument required\n", 29);
+        command_free(cmd_arry);
+        exit(2);
+    }
+
+    command_free(cmd_arry);
+    exit((int)status);
 
 	return (1);
 }
